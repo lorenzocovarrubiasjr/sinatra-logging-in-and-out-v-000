fc544592faaf700d1e77c1a2dfa39cea1642210a
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-04-22 18:35:44 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,7 +3,7 @@ require 'sinatra/base'
 class Helpers
 
   def current_user(session)
-    @user = User.find(session[:user_id])
+    @user = User.find_by_id(session[:user_id])
   end
 
   def is_logged_in?(session)
